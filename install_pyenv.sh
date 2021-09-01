c5aaf679b22245db91fcbe133f23c1f6d75e6f9f
theartist <theartist@gmail.com>
2021-08-31 22:26:08 -0700

    Update install_pyenv.sh

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -37,8 +37,8 @@ if [ -d ~/.pyenv/ ]; then
 else
   echo "git clone https://github.com/pyenv/pyenv.git ~/.pyenv"
   git clone https://github.com/pyenv/pyenv.git ~/.pyenv 
-  echo 'export PYENV_ROOT="$HOME/.pyenv"' > ~/.bashrc
-  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' > ~/.bashrc
+  echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+  echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
   echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
   source ~/.bashrc
   echo
