0695e1f31022fb0ebbc910b9bba890526c22c425
thraddash <theartist@gmail.com>
2021-11-03 15:23:39 -0700

    reload bashrc

--- install_pyenv.sh
+++ install_pyenv.sh
@@ -41,6 +41,7 @@ else
   echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
   echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
   source ~/.bashrc
+  . ~/.bashrc
   echo
 fi
 
